7135b07dd8c7ddc9b9814f4339278848aa540893
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-02-06 14:31:19 +0530

    added scrollevent anf part 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import Experience from './components/Experience'
+import { ScrollControls } from '@react-three/drei'
 
 const App = () => {
   return (
     <>
     <Canvas>
       <color attach="background" args={["#ececec"]} />
+      <ScrollControls pages={5} damping={0.3} >
       <Experience/>
+      </ScrollControls>
     </Canvas>
     </>
   )
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,105 @@
-import { Float, OrbitControls } from '@react-three/drei'
-import React from 'react'
+import { Float, OrbitControls, PerspectiveCamera, useScroll } from '@react-three/drei'
+import React, { useMemo, useRef } from 'react'
 import Background from './Background'
 import { Airplane } from './Airplane'
 import { Cloud } from './Cloud'
+import  * as THREE  from 'three';
+import { useFrame } from '@react-three/fiber'
+
+const LINE_NB_POINTS = 2000;
 
 const Experience = () => {
+
+  const curve = useMemo(()=>{
+    return new THREE.CatmullRomCurve3([
+      new THREE.Vector3(0,0,0),
+      new THREE.Vector3(0,0,-10),
+      new THREE.Vector3(-2,0,-20),
+      new THREE.Vector3(-3,0,-30),
+      new THREE.Vector3(0,0,-40),
+      new THREE.Vector3(5,0,-50),
+      new THREE.Vector3(7,0,-60),
+      new THREE.Vector3(5,0,-70),
+      new THREE.Vector3(0,0,-80),
+      new THREE.Vector3(0,0,-90),
+      new THREE.Vector3(0,0,-100),
+    ],false,"catmullrom",0.5)
+  })
+
+  const shape = useMemo (()=>{
+    const shape = new THREE.Shape();
+    shape.moveTo(0,-0.2);
+    shape.lineTo(0,0.2);
+
+    return shape;
+  },[curve])
+
+  const linePoints = useMemo(()=>{
+    return curve.getPoints(LINE_NB_POINTS);
+  },[curve]);
+
+  const cameraRef = useRef();
+  const scroll = useScroll();
+  const airplane = useRef();
+
+  useFrame((_state,delta)=>{
+    
+    const curPointIndex = Math.min(
+      Math.round(scroll.offset * linePoints.length), linePoints.length - 1
+    )
+    const curPoint = linePoints[curPointIndex];
+    const pointAhead =  linePoints[Math.min(curPointIndex+1, linePoints.length)]
+
+    const xDisplacement = (pointAhead.x - curPoint.x) * 80;
+
+    const angleRotation = (xDisplacement < 0 ? 1: -1)*
+    Math.min(Math.abs(xDisplacement),Math.PI / 3);
+
+    const targetAirplaneQuaterion = new THREE.Quaternion().setFromEuler(
+      new THREE.Euler(
+        airplane.current.rotation.x,
+        airplane.current.rotation.y,
+        angleRotation,
+
+      )
+    )
+
+    airplane.current.quaternion.slerp(targetAirplaneQuaterion, delta * 2)
+
+    cameraRef.current.position.lerp(curPoint,delta * 24)
+  })
+
+  
+
+
   return (
     <>
-    <OrbitControls/>
+    
+
+    <group ref={cameraRef} >
     <Background/>
+    <PerspectiveCamera position={[0,0,5]} fov={30}  makeDefault />
+    <group ref={airplane} >
     <Float floatIntensity={2}  speed={2} >
     <Airplane rotation-y={Math.PI / 2} scale={[0.2,0.2,0.2]} position-y={0.1} />
     </Float>
+    </group>
+    </group>
+
+    <group position-y={-2} >
+    <mesh>
+      <extrudeGeometry args={[
+        shape,
+        {
+          steps: LINE_NB_POINTS,
+          bevelEnabled: false,
+          extrudePath: curve,
+        }
+      ]} />
+
+      <meshStandardMaterial color={"white"} opacity={0.7} transparent />
+    </mesh>
+    </group>
 
     <Cloud opacity={0.7} scale={[0.3,0.3, 0.4]} position={[-3,1,-3]} />
     <Cloud opacity={0.7} scale={[0.3,0.3, 0.4]} position={[3.5,-0.5,-2]} />
